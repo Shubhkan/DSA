db15a505ccc484d276f5b488dd24ddb518811d3e
Shubhkant Choudary <160928541+Shubhkan@users.noreply.github.com>
2026-07-03 21:54:47 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.16%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+          s = s.trim();
+
+       
+        int index = s.lastIndexOf(' ');
+          return  s.length()-index-1;
+        
+    }
+}
